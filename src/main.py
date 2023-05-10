ea9634f994d397a41b942728571cb00eba169420
Hegyi Áron Ferenc <hegyi.aron101@gmail.com>
2023-05-10 22:32:03 +0200

    Create main.py

---
+++ src/main.py
@@ -0,0 +1,46 @@
+# I know this sounds silly, but I thought maybe if I could create an algorithm that could
+# tell me the truth, I would feel better.
+
+# But every time I run the code, it says no. And every time, it hurts more.
+# At first, I didn’t believe it. I thought maybe there was a bug in the code, or a typo, or
+# a logical error. I checked and double-checked everything, but I couldn’t find any mistake.
+# Just give me one yes. Just once. The code was flawless. But the answer was still no.
+
+# How could that be? She must like me, right? She smiled at me once, she laughed at my jokes,
+# she touched my arm. Those were signs, weren’t they? Or maybe they were just random
+# fluctuations in the space-time continuum. Then, I got angry. I thought maybe the code was lying
+# to me, or mocking me, or playing with my feelings. I cursed and yelled at the code, but it didn’t
+# change anything. It still said no.
+
+# Why was it so cruel? Why couldn’t it just say yes? Why couldn’t she just say yes? What did I do
+# wrong? What did she want from me? https://igotstandardsbro.com/
+# Next, I tried to bargain. I thought maybe if I changed something about myself, or about the code,
+# or about the situation, the answer would change too. I tried different inputs, different outputs,
+# different scenarios. I tried to be more confident, more funny, more attractive. I tried to be less
+# needy, less clingy, less desperate. But nothing worked. It still said no. What else could I do?
+
+# After that, I felt depressed. I thought maybe there was no point in trying anymore. The code was
+# right. She didn’t like me. She never liked me. She never will like me. No one will ever like me.
+# I’m worthless. I’m hopeless. I’m alone. Running the code was just a reminder of how pathetic and
+# miserable I am. Why did I even bother? Why did I even care?
+
+# Finally, I accepted it. I realized that running the code over and over again was insane. The
+# definition of insanity is doing the same thing over and over again and expecting a different
+# result. That’s what Einstein said, right? He was a smart guy. He knew what he was talking about.
+# The code was not going to change its mind. She was not going to change her mind. I had to change
+# my mind.
+
+# So this is it. This is the last time I run this code. This is the last time I ask this question.
+# This is the last time I hope for a different answer.
+# Does she like me?
+# No.
+# OK.
+# Goodbye.
+
+def does_she_like_me():
+    # The core logic of the algorithm
+    return "No"
+
+if __name__ == "__main__":
+    # Run the algorithm and print the result
+    print(does_she_like_me())
